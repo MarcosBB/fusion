1584ab5600473ca02c684f7666cc1623751a065a
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-06-02 20:35:24 -0400

    Separando partes do template e criando paginas de erro 404 e 500
    aula 53

--- core/views.py
+++ core/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
     
+class Erro404View(TemplateView):
+    template_name = '404.html'
+
+class Erro500View(TemplateView):
+    template_name = '500.html'
