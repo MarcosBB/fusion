e2b4336ea345c6dfe3478d0fd1564f9430969494
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-06-09 15:23:11 -0400

    Configurando como vai aparecer na página de admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Servico, Funcionario
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome','cargo', 'ativo', 'modificado')
