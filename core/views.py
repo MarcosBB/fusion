6ce6261b942ffc218d0d8631468d72e739e16962
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-06-21 11:04:34 -0400

    Adicionando funcionalidade de e-mail

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import TemplateView
+from django.contrib import messages
+from core.forms import ContatoForm
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 
 from .models import Servico, Funcionario, Recurso
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
+
     
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -11,3 +17,12 @@ class IndexView(TemplateView):
         context['funcionarios'] = Funcionario.objects.order_by('?').all()
         context['recursos'] = Recurso.objects.order_by('?').all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso!!!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
--- fusion/settings.py
+++ fusion/settings.py
@@ -131,3 +131,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email teste console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+'''
+# Email produção
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no.reply@fusion.com.br'
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+EMAIL_HOST_PASSWORD = 'fusion'
+DEFAULT_FROM_EMAIL = 'contato@fusion.com
+'''
